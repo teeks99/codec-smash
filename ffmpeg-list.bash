ac13670776f084ff53990d4bc7a8d42d8a911a02
Thomas Kent <teeks99@yahoo.com>
2016-05-11 21:57:02 -0500

    Updated to Xenial, matching features to ubuntu core as closely as possible

--- ffmpeg-list.bash
+++ ffmpeg-list.bash
@@ -1,16 +1,19 @@
 repo=$1
 ver=`lsb_release -c -s`
+dir=ffmpeg-features-$ver-$repo
 
 if [ "$repo" == "" ]; then
     echo Need to input the repo type/name as an argument
 else
     
-ffmpeg 2> ffmpeg-header-$ver-$repo.txt
-ffmpeg -formats > ffmpeg-formats-$ver-$repo.txt
-ffmpeg -codecs > ffmpeg-codecs-$ver-$repo.txt
-ffmpeg -bsfs > ffmpeg-bsfs-$ver-$repo.txt
-ffmpeg -protocols > ffmpeg-protocols-$ver-$repo.txt
-ffmpeg -filters > ffmpeg-filters-$ver-$repo.txt
-ffmpeg -pix_fmts > ffmpeg-pix_fmts-$ver-$repo.txt
+
+mkdir $dir
+ffmpeg 2> $dir/header.txt
+ffmpeg -formats > $dir/formats.txt
+ffmpeg -codecs > $dir/codecs.txt
+ffmpeg -bsfs > $dif/bsfs.txt
+ffmpeg -protocols > $dir/protocols.txt
+ffmpeg -filters > $dir/filters.txt
+ffmpeg -pix_fmts > $dir/pix_fmts.txt
 
 fi
--- ffmpeg-setup.bash
+++ ffmpeg-setup.bash
@@ -4,12 +4,12 @@
 # Set version numbers here...use DIST to indicate that you aren't building it and to use the repo version
 ffmpeg_version=3.0
 x264_version=DIST # 40bb56814e56ed342040bdbf30258aab39ee9e89
-x265_version=1.9
-vpx_version=1.5.0
+x265_version=DIST #1.9
+vpx_version=DIST #1.5.0
 
 # Pause between sections
-#PAUSE=False
-PAUSE=True
+PAUSE=False
+#PAUSE=True
 
 sudo echo "" # this will ask you for your password immediately, should cache it for the rest of the process
 sys_ver=`lsb_release -c -s`
@@ -25,11 +25,13 @@ pkg_build="git-core mercurial checkinstall yasm build-essential cmake texi2html 
 
 # IO and Misc
 pkg_io=
+pkg_io=$pkg_io" ladspa-sdk-dev" # HW Accel
 pkg_io=$pkg_io" libvdpau-dev" # HW Accel
 pkg_io=$pkg_io" libva-dev" # HW Accel
 pkg_io=$pkg_io" libx11-dev libxfixes-dev" # Grab X11
 pkg_io=$pkg_io" libsdl1.2-dev" # Grab video framebuffer
 pkg_io=$pkg_io" libdc1394-22-dev" # Firewire grabbing
+#pkg_io=$pkg_io" libiec61883-dev" # ICE 61883 Firewire grabbing # FFMpeg can't find lib
 pkg_io=$pkg_io" libopenal-dev" # Audio IO
 pkg_io=$pkg_io" libpulse-dev" # Audio IO
 pkg_io=$pkg_io" libjack-jackd2-dev" # Connections to/from JACK sound server
@@ -44,7 +46,13 @@ pkg_io=$pkg_io" frei0r-plugins-dev" # Filtering
 pkg_io=$pkg_io" libcv-dev" # Open CV Filters+
 pkg_io=$pkg_io" libfftw3-dev" # Fast Fourier Transform?
 pkg_io=$pkg_io" librtmp-dev" # Streams over RTMP
-# TODO: OpenCL? VDA?
+pkg_io=$pkg_io" libbs2b-dev" # bs2b DSP library
+pkg_io=$pkg_io" libcdio-paranoia-dev" # CD Drive reading
+pkg_io=$pkg_io" libsnappy-dev" # Snappy compression, needed for hap encoding
+pkg_io=$pkg_io" libsoxr-dev" # Resampling
+pkg_io=$pkg_io" libssh-dev" # SFTP
+#pkg_io=$pkg_io" libzmq-dev" # message passing via libzmq  # FFMpeg can't find lib
+pkg_io=$pkg_io" libzvbi-dev" # teletext support
 
 pkg_enc=
 # Subtitles
@@ -52,6 +60,7 @@ pkg_enc=$pkg_enc" libass-dev" # SubStation Alpha Subtitles
 # Audio
 pkg_enc=$pkg_enc" libmp3lame-dev" # MP3
 pkg_enc=$pkg_enc" libtwolame-dev" # MP2
+pkg_enc=$pkg_enc" libwavpack-dev" # wavpack encoding
 pkg_enc=$pkg_enc" libfdk-aac-dev" # AAC - best
 pkg_enc=$pkg_enc" libfaac-dev" # AAC - next best
 pkg_enc=$pkg_enc" libvo-aacenc-dev" # AAC - worst
@@ -65,11 +74,15 @@ pkg_enc=$pkg_enc" libopencore-amrnb-dev libopencore-amrwb-dev" # Adaptive Multi-
 pkg_enc=$pkg_enc" libvo-amrwbenc-dev"  # Adaptive Multi-Rate
 pkg_enc=$pkg_enc" libgsm1-dev" # # GSM 06.10
 pkg_enc=$pkg_enc" libmodplug-dev" # MOD format
+pkg_enc=$pkg_enc" libgme-dev" # Game Music Emulation
+pkg_enc=$pkg_enc" libshine-dev" # fixed-point MP3 encoding
 # Video
 pkg_enc=$pkg_enc" libtheora-dev" # Theora
 pkg_enc=$pkg_enc" libopenjpeg-dev" # JPEG2000
 pkg_enc=$pkg_enc" libschroedinger-dev" # Schroedinger Dirac
 pkg_enc=$pkg_enc" libxvidcore-dev" # XVid
+pkg_enc=$pkg_enc" libopencv-dev" # video filtering OpenCV
+pkg_enc=$pkg_enc" libwebp-dev" # WebP
 if [ "$x264_version" == "DIST" ]; then
   pkg_enc=$pkg_enc" libx264-dev" # H.264
 fi
@@ -81,62 +94,75 @@ if [ "$vpx_version" == "DIST" ]; then
 fi
 
 #ffmpeg config options
-cfg_opts=$cfg_opts" --enable-gpl --enable-version3 --enable-nonfree"
-cfg_opts=$cfg_opts" --enable-runtime-cpudetect"
-cfg_opts=$cfg_opts" --enable-vdpau --enable-vaapi --enable-vda"
-#cfg_opts=$cfg_opts" --enable-opencl"
-# Grab/Compress
-cfg_opts=$cfg_opts" --enable-x11grab --enable-libdc1394"
-cfg_opts=$cfg_opts" --enable-openal"
-cfg_opts=$cfg_opts" --enable-libpulse"
-cfg_opts=$cfg_opts" --enable-libv4l2"
-cfg_opts=$cfg_opts" --enable-libbluray"
-cfg_opts=$cfg_opts" --enable-libflite"
-cfg_opts=$cfg_opts" --enable-bzlib --enable-zlib"
-# Filters/etc
-cfg_opts=$cfg_opts" --enable-frei0r"
-cfg_opts=$cfg_opts" --enable-libfreetype"
-cfg_opts=$cfg_opts" --enable-fontconfig"
-#cfg_opts=$cfg_opts" --enable-libopencv"
-cfg_opts=$cfg_opts" --enable-librtmp"
-#cfg_opts=$cfg_opts" --enable-gnutls"
-#cfg_opts=$cfg_opts" --enable-openssl"
-# Subtitles
-cfg_opts=$cfg_opts" --enable-libass"
-# Audio
-cfg_opts=$cfg_opts" --enable-libmp3lame"
-cfg_opts=$cfg_opts" --enable-libtwolame"
-cfg_opts=$cfg_opts" --enable-libfdk-aac"
-cfg_opts=$cfg_opts" --enable-libfaac"
-cfg_opts=$cfg_opts" --enable-libvorbis"
-cfg_opts=$cfg_opts" --enable-libspeex"
-cfg_opts=$cfg_opts" --enable-libopus"
-cfg_opts=$cfg_opts" --enable-libopencore-amrnb --enable-libopencore-amrwb"
-cfg_opts=$cfg_opts" --enable-libvo-amrwbenc"
-cfg_opts=$cfg_opts" --enable-libgsm"
-cfg_opts=$cfg_opts" --enable-libmodplug"
-# Video
-cfg_opts=$cfg_opts" --enable-libtheora"
-cfg_opts=$cfg_opts" --enable-libopenjpeg"
-cfg_opts=$cfg_opts" --enable-libschroedinger"
-cfg_opts=$cfg_opts" --enable-libxvid"
-if [ "$x264_version" != "NONE" ]; then
-  cfg_opts=$cfg_opts" --enable-libx264"
-fi
-if [ "$x265_version" != "NONE" ]; then
-  cfg_opts=$cfg_opts" --enable-libx265"
-fi
-if [ "$vpx_version" != "NONE" ]; then
-  cfg_opts=$cfg_opts" --enable-libvpx"
-fi
-cfg_opts=$cfg_opts" "
-
+cfg_opts=$cfg_opts" --enable-gpl" # Allows: frei0r, libcdio, libx264, libx265, libxvid, x11grab
+cfg_opts=$cfg_opts" --enable-nonfree" # Allows: libfaac, libfdk_aac
+cfg_opts=$cfg_opts" --enable-version3" # Allows: libopencore-amrnb, libopencore-amrwb, libvo-amrwbenc
+cfg_opts=$cfg_opts" --enable-shared"
+cfg_opts=$cfg_opts" --enable-avresample"
 cfg_opts=$cfg_opts" --extra-libs=-ldl" # Dynamic libc?
 
+# External Library Support
+cfg_opts=$cfg_opts" --enable-fontconfig" # fontconfig, useful for drawtext filter
+cfg_opts=$cfg_opts" --enable-avisynth" # AviSynth script files
+cfg_opts=$cfg_opts" --enable-frei0r" # frei0r video filtering
+cfg_opts=$cfg_opts" --enable-gnutls" # https support
+cfg_opts=$cfg_opts" --enable-ladspa" # LADSPA audio filtering
+cfg_opts=$cfg_opts" --enable-libass" # libass subtitles rendering, needed for subtitles and ass filter
+cfg_opts=$cfg_opts" --enable-libbluray" # BluRay Disc support (no encryption)
+cfg_opts=$cfg_opts" --enable-libbs2b" # bs2b DSP library
+cfg_opts=$cfg_opts" --enable-libcaca" # textual display using libcaca
+cfg_opts=$cfg_opts" --enable-libcdio" # audio CD grabbing
+cfg_opts=$cfg_opts" --enable-libdc1394" # IIDC-1394 grabbing using libdc1394 and libraw1394
+cfg_opts=$cfg_opts" --enable-libfaac" # AAC encoding via libfaac
+cfg_opts=$cfg_opts" --enable-libfdk-aac" # AAC de/encoding via libfdk-aac (preferred AAC)
+cfg_opts=$cfg_opts" --enable-libflite" # flite (voice synthesis) support
+cfg_opts=$cfg_opts" --enable-libfreetype" # libfreetype, needed for drawtext filter
+cfg_opts=$cfg_opts" --enable-libfribidi" # libfribidi, improves drawtext filter
+cfg_opts=$cfg_opts" --enable-libgme" # Game Music Emulation
+cfg_opts=$cfg_opts" --enable-libgsm" # GSM de/encoding
+#cfg_opts=$cfg_opts" --enable-libiec61883" # ICE 61883 Firewire grabbing # FFMpeg can't find lib
+cfg_opts=$cfg_opts" --enable-libmodplug" # ModPlug
+cfg_opts=$cfg_opts" --enable-libmp3lame" # MP3 encoding
+cfg_opts=$cfg_opts" --enable-libopencore-amrnb" # AMR-NB de/encoding
+cfg_opts=$cfg_opts" --enable-libopencore-amrwb" # AMR-WB decoding
+cfg_opts=$cfg_opts" --enable-libopencv" # video filtering via libopencv
+cfg_opts=$cfg_opts" --enable-libopenjpeg" # JPEG 2000 de/encoding
+cfg_opts=$cfg_opts" --enable-libopus" # Opus de/encoding
+cfg_opts=$cfg_opts" --enable-libpulse" # Pulseaudio input
+cfg_opts=$cfg_opts" --enable-librtmp" # RTMP[E] support
+cfg_opts=$cfg_opts" --enable-libschroedinger" # Dirac de/encoding via libschroedinger
+cfg_opts=$cfg_opts" --enable-libshine" # fixed-point MP3 encoding
+cfg_opts=$cfg_opts" --enable-libsnappy" # Snappy compression, needed for hap encoding
+cfg_opts=$cfg_opts" --enable-libsoxr" # Include libsoxr resampling 
+cfg_opts=$cfg_opts" --enable-libspeex" # Speex de/encoding
+cfg_opts=$cfg_opts" --enable-libssh" # SFTP
+cfg_opts=$cfg_opts" --enable-libtheora" # Theora encoding
+cfg_opts=$cfg_opts" --enable-libtwolame" # MP2 encoding
+cfg_opts=$cfg_opts" --enable-libv4l2" # libv4l2/v4l-utils
+cfg_opts=$cfg_opts" --enable-libvo-amrwbenc" # AMR-WB encoding
+cfg_opts=$cfg_opts" --enable-libvorbis" # Vorbis en/decoding
+cfg_opts=$cfg_opts" --enable-libwebp" # WebP encoding
+if [ "$vpx_version" != "NONE" ]; then # VP8 and VP9 de/encoding 
+  cfg_opts=$cfg_opts" --enable-libvpx"
+fi
+cfg_opts=$cfg_opts" --enable-libwavpack" # Vorbis en/decoding
+if [ "$x264_version" != "NONE" ]; then # H.264 Encoding
+  cfg_opts=$cfg_opts" --enable-libx264"
+fi
+if [ "$x265_version" != "NONE" ]; then # H.265/HEVC Encoding
+  cfg_opts=$cfg_opts" --enable-libx265"
+fi
+cfg_opts=$cfg_opts" --enable-libxvid" # Xvid encoding via xvidcore, native MPEG-4/Xvid encoder exists
+# cfg_opts=$cfg_opts" --enable-libzmq" # message passing via libzmq # FFMpeg can't find lib
+cfg_opts=$cfg_opts" --enable-libzvbi" # teletext support
+cfg_opts=$cfg_opts" --enable-openal" # OpenAL 1.1 capture support
+#cfg_opts=$cfg_opts" --enable-opencl" # OpenCL code # FFMpeg can't find on ubuntu
+cfg_opts=$cfg_opts" --enable-opengl" # OpenGL rendering
+#cfg_opts=$cfg_opts" --enable-openssl" # needed for https support if gnutls is not used
+cfg_opts=$cfg_opts" --enable-x11grab" # X11 grabbing (legacy)
+
 # Don't add
-# --enable-avisynth - Windows only, requires vfw32
 # --enable-libaacplus - not available on ubuntu?
-# --enable-libcdio - something isn't right about the ubutu version "libavdevice/libcdio.c:26:23: fatal error: cdio/cdda.h: No such file or directory"
 # --enable-libnut - needs to be built from source on ubuntu, there is a good nut muxer built in, I believe;
 # --enable-libxavs - ??
 # --enable-openssl - What is this for?
